ada22541aa088b2311f48053b1c6b15fb6eaaf4b
Pablo Macedo <pablomacedoribeiro@gmail.com>
2026-05-26 20:21:27 -0300

    Chore: Adiciona o setup inicial da estrutura do backend e DB.

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Usuario, Equipamento, Manutencao, Agendamento
+
+admin.site.register(Usuario)
+admin.site.register(Equipamento)
+admin.site.register(Manutencao)
+admin.site.register(Agendamento)
--- backend/core/models.py
+++ backend/core/models.py
@@ -1,3 +1,47 @@
 from django.db import models
 
-# Create your models here.
+
+class Usuario(models.Model):
+    nome = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return self.nome
+
+
+class Equipamento(models.Model):
+    nome = models.CharField(max_length=100)
+    patrimonio = models.CharField(max_length=50, unique=True)
+    localizacao = models.CharField(max_length=100)
+    status = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.nome
+
+
+class Manutencao(models.Model):
+    equipamento = models.ForeignKey(
+        Equipamento,
+        on_delete=models.CASCADE
+    )
+    descricao = models.TextField()
+    data = models.DateField()
+
+    def __str__(self):
+        return f"Manutenção #{self.id}"
+
+
+class Agendamento(models.Model):
+    usuario = models.ForeignKey(
+        Usuario,
+        on_delete=models.CASCADE
+    )
+    equipamento = models.ForeignKey(
+        Equipamento,
+        on_delete=models.CASCADE
+    )
+    data = models.DateField()
+
+    def __str__(self):
+        return f"Agendamento #{self.id}"
+    
