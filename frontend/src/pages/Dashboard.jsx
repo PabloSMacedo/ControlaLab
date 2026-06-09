f95bdce8e7b855eb4b8fea29c37108d17be7822f
schuhcamile <camileschuh@hotmail.com>
2026-06-08 22:19:06 -0300

    teste-frotend

---
+++ frontend/src/pages/Dashboard.jsx
@@ -0,0 +1,15 @@
+function Dashboard() {
+  return (
+    <div style={{ padding: "30px" }}>
+      <h1>Dashboard</h1>
+
+      <h3>Resumo do Laboratório</h3>
+
+      <p>Equipamentos cadastrados: 25</p>
+      <p>Manutenções pendentes: 4</p>
+      <p>Laboratórios ativos: 3</p>
+    </div>
+  );
+}
+
+export default Dashboard;
